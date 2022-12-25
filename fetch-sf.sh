1ddb92c11ddd50553bfe716cef026a379bf7a546
Konrad Paluszek <kezsulap@gmail.com>
2022-12-25 14:08:30 +0100

    Clean up after compiling Stockfish

--- fetch-sf.sh
+++ fetch-sf.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/ianfab/Fairy-Stockfish.git
-make -C Fairy-Stockfish/src/ build
+make -C Fairy-Stockfish/src/ build;
+mv Fairy-Stockfish/src/stockfish .
+rm -rf Fairy-Stockfish
